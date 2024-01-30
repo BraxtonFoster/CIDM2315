168f9460fb1490d9d0414911a1e0fbc7942279e5
BraxtonFoster <157443382+BraxtonFoster@users.noreply.github.com>
2024-01-29 23:34:02 -0600

    Add files via upload

---
+++ Homework2pt2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter three numbers:");
+
+            Console.Write("Please input the first num: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please input the second num: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please input the third num: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            double smallest = FindSmallest(num1, num2, num3);
+
+            Console.WriteLine($"The smallest value is: {smallest}");
+        }
+
+        static double FindSmallest(double a, double b, double c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+    }
+    }
