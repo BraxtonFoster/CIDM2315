641889c2d368bd9c939a377729205f679ae20f60
BraxtonFoster <157443382+BraxtonFoster@users.noreply.github.com>
2024-02-05 22:36:45 -0600

    Add files via upload

---
+++ Homework3pt1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Homework3
+{
+    class PrimeChecker
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a integer: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (IsPrime(N))
+            {
+                Console.WriteLine($"{N} is prime");
+            }
+            else
+            {
+                Console.WriteLine($"{N} is non-prime");
+            }
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Homework3pt3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework3
+{
+    class SquarePattern
+    {
+        static void Main()
+        {
+            Console.Write("Enter the value of N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N > 0)
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    for (int j = 1; j <= N; j++)
+                    {
+                        if (j <= i)
+                        {
+                            Console.Write("* ");
+                        }
+                        else
+                        {
+                            Console.Write("  "); 
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a positive integer for N.");
+            }
+        }
+    }
+}
