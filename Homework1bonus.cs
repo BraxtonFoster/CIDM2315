f1cc1e3c3c053ebd89f49d1decbfe97f03ea4938
BraxtonFoster <157443382+BraxtonFoster@users.noreply.github.com>
2024-06-17 18:19:10 -0500

    Add files via upload

---
+++ Homework1bonus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework1bonus
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please input a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+    }
+}
