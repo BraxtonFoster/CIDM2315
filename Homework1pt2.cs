0f9f9e43228beb2871001f721783ef738a3128a0
BraxtonFoster <157443382+BraxtonFoster@users.noreply.github.com>
2024-06-17 18:18:21 -0500

    Add files via upload

---
+++ Homework1pt2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework1pt2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter three numbers:");
+
+            Console.Write("Number 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 3: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int smallest = num1;
+
+            if (num2 < smallest)
+            {
+                smallest = num2;
+            }
+
+            if (num3 < smallest)
+            {
+                smallest = num3;
+            }
+
+            Console.WriteLine($"The smallest number is: {smallest}");
+        }
+    }
+}
+
