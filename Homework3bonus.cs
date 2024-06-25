f666031af35c1d32b68eb82c8e37ba0ccf29f40e
BraxtonFoster <157443382+BraxtonFoster@users.noreply.github.com>
2024-06-24 23:22:05 -0500

    Add files via upload

---
+++ Homework3bonus.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework3bonus;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the value of N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
