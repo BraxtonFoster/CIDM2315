8fdefa71531dee8e58a1c6bac8224049b7382056
BraxtonFoster <157443382+BraxtonFoster@users.noreply.github.com>
2024-06-24 23:19:36 -0500

    Add files via upload

---
+++ Homework3pt2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the value of N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+                
+                Console.Write("* ");
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
