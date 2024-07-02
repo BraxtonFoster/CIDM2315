f7d2537326c298625088a80773ff45f1b07e1079
BraxtonFoster <157443382+BraxtonFoster@users.noreply.github.com>
2024-07-01 22:00:43 -0500

    Add files via upload

---
+++ Homework4.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework4;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("a = ");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("; b = ");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        int largestNumber = GetLargestNumber(firstNumber, secondNumber);
+        Console.WriteLine($"The largest number is: {largestNumber}");
+    }
+
+    static int GetLargestNumber(int num1, int num2)
+    {
+        return (num1 > num2) ? num1 : num2;
+    }
+}
